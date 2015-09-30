60b8a842703afabfe2b6ce0a554cda38f547e7a2
tbrenneison <tori.brenneison@gmail.com>
2015-09-29 23:47:50 -0400

    minor changes

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string line = "6g52M lQw6 s I k,6g52M lQw6 s I k"; //test input
+
+            string[] splitLine = line.Split(',');
+     
+            string partone = splitLine[0];
+            int index = partone.LastIndexOf(splitLine[1]); //look right-left for last index of part two in part one
+
+            if (index == -1)
+            { Console.WriteLine("0"); }
+            else
+            { Console.WriteLine("1"); }
+
+
+
+
+
         }
     }
 }
