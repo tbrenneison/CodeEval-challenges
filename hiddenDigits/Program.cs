b499e5991613f40a246873a252771ed44871deb6
tbrenneison <tori.brenneison@gmail.com>
2015-10-12 16:35:11 -0400

    added hiddenDigits and reverseGroups

---
+++ hiddenDigits/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hiddenDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            In this challenge you're given a random string containing hidden and visible digits. 
+            The digits are hidden behind lower case latin letters as follows: 0 is behind 'a', 1 is behind ' b ' etc., 9 is behind 'j'. 
+            Any other symbol in the string means nothing and has to be ignored. 
+            So the challenge is to find all visible and hidden digits in the string and print them out in order of their appearance.
+            */
+
+            string line = "(ABW>'yy^'M{X-K}q,"; //test input
+
+            string alphabet = "abcdefghij";
+
+            int[] numbers = new int[alphabet.Length];
+
+            for (int i = 0; i <= alphabet.Length-1; i++) //put numbers 0-25 in an array
+            {
+                numbers[i] = i; 
+            }
+
+            StringBuilder result = new StringBuilder(); 
+
+            foreach (char c in line) //for each character in "line"
+            {
+                if(char.IsLower(c)) //if it's a lowercase letter
+                    {
+                    if (alphabet.IndexOf(c) != -1) //if it's in the abbreviated alphabet 
+                         {
+                        int index = alphabet.IndexOf(c); //find the hidden digit 
+                        result.Append(index);
+                         }
+                    }
+                else if(char.IsDigit(c)) //if it's a digit
+                {
+                    if(c <= '9') //and is less than nine 
+                    {
+                        result.Append(c); 
+                    }
+                }
+
+            }
+
+            string hiddenDigits = result.ToString(); 
+
+            if(hiddenDigits.Length == 0) //if the string is empty (no digits)
+            {
+                Console.WriteLine("NONE");
+            }
+            else //otherwise write them out
+            {
+                Console.WriteLine(hiddenDigits);
+            }
+
+
+        }
+    }
+}
---
+++ reverseGroups/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseGroups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            Given a list of numbers and a positive integer k, reverse the elements of the list, k items at a time. 
+            If the number of elements is not a multiple of k, then the remaining items in the end should be left as is.
+            Each line in this file contains a list of numbers and the number k, separated by a semicolon.
+            The list of numbers are comma delimited.
+            Print out the new comma separated list of numbers obtained after reversing.
+            */
+
+            string line = "1,2,3,4,5;2"; //test input (should output 2,1,4,3,5)
+
+            string[] firstSplit = line.Split(';'); //splits at semicolon
+            //"1,2,3,4,5", "2"
+
+            int thisMany = int.Parse(firstSplit[firstSplit.Length - 1]); //reverse this many at at time
+
+            string[] secondSplit = firstSplit[0].Split(','); //12345
+
+          
+
+            
+
+
+        }
+    }
+}
